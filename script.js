aedfd48f601f116db4ade63adc2721ce2fbd40ac
NIXONHS <111463005+NIXONHS1@users.noreply.github.com>
2024-06-12 14:48:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function respuesta(respuesta) {
+    if (respuesta === 'si') {
+      alert('Disculpame por todo TE AMO MUCHO ❤️');
+    } else if (respuesta === 'no') {
+      alert('Entiendo. Espero que cambies de opinión pronto');
+    }
+  }
