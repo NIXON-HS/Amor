74be324922dc66fcd73696f2eec853a236f8c63b
NIXONHS <111463005+NIXONHS1@users.noreply.github.com>
2024-06-12 15:08:12 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function respuesta(respuesta) {
-    if (respuesta === 'si') {
-      alert('Disculpame por todo TE AMO MUCHO ❤️');
-    } else if (respuesta === 'no') {
-      alert('Entiendo. Espero que cambies de opinión pronto');
-    }
-  }
